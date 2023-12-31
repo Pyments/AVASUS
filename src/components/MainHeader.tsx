f14a3903a5e9bd91c49d798e356c997dd3da2828
Eduardo-Pyment <edupymota@protonmail.com>
2023-12-31 11:17:31 -0300

    Basic react-router-dom setup

--- src/components/MainHeader.tsx
+++ src/components/MainHeader.tsx
@@ -1,19 +1,22 @@
 import "../style/MainHeader.scss";
+import { Link } from "react-router-dom";
 
 const MainHeader = () => {
   return (
     <>
       <header className="main-header">
         <ul className="list-group">
-          <li className="list-group-item">
-            <img src="#" alt="Logo AVASUS" />
-          </li>
-          <li className="list-group-item">Início</li>
+          <Link to="/">
+            <li className="list-group-item">
+              <img src="#" alt="Logo AVASUS" />
+            </li>
+          </Link>
+          <Link to="/"><li className="list-group-item">Início</li></Link>
           <li className="list-group-item">Sobre Nós</li>
           <li className="list-group-item">Cursos</li>
           <li className="list-group-item">Parceiros</li>
           <li className="list-group-item">Transparência</li>
-          <li className="list-group-item">Contato</li>
+          <Link to="/contact"><li className="list-group-item">Contato</li></Link>
           <li className="list-group-item header-searchbar">===Searchbar===</li>
           <li className="list-group-item">Entrar</li>
           <li className="list-group-item">Cadastrar</li>
