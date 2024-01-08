1909e5b6fa1e8a36e19ae1149bb6fc1d8053d57f
Eduardo-Pyment <edupymota@protonmail.com>
2024-01-08 15:29:10 -0300

    Parceiros_big pagination (arrows) done

--- src/components/Parceiros_big.tsx
+++ src/components/Parceiros_big.tsx
@@ -1,20 +1,27 @@
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, keepPreviousData } from "@tanstack/react-query";
+import { useState } from "react";
+
+import ArrowLeft from "../assets/feather/arrow-left.svg";
+import ArrowRight from "../assets/feather/arrow-right.svg";
+
 import "../style/components/Parceiros_big.scss";
 
-const Parceiros_big =() => {
-  const fetchParceiros = async () => {
+const Parceiros_big = () => {
+  const [page, setPage] = useState(1);
+
+  const fetchParceiros = async (page = 1) => {
     const response = await fetch(
-      `http://0.0.0.0:3004/parceiros?_page=1&_limit=9`
+      `http://0.0.0.0:3004/parceiros?_page=${page}&_limit=9`
     );
     const data = await response.json();
     return data;
   };
 
   const { isLoading, error, data, isFetching } = useQuery({
-    queryKey: ["queryParceirosBig"],
-    queryFn: () => fetchParceiros()
+    queryKey: ["queryParceirosBig", page],
+    queryFn: () => fetchParceiros(page),
+    placeholderData: keepPreviousData,
   });
-  
 
   if (isLoading || isFetching) {
     return (
@@ -34,7 +41,7 @@ const Parceiros_big =() => {
     <>
       <section className="parceiros">
         <h2 className="parceiros-cabecalho">Nossos parceiros</h2>
-        <span className="parceiros-resultados">6 de # resultados</span>
+        <span className="parceiros-resultados">9 de # resultados</span>
         <ol className="parceiros-lista">
           {data?.map((data: any) => {
             return (
@@ -49,10 +56,25 @@ const Parceiros_big =() => {
             );
           })}
         </ol>
-        
+        <span className="parceiros-page-selector">
+          <button
+            onClick={() => setPage((old) => Math.max(old - 1, 1))}
+            disabled={page === 1}
+          >
+            <img src={ArrowLeft} />
+          </button>
+          {page}
+          <button
+            onClick={() => {
+              setPage((old) => old + 1);
+            }}
+          >
+            <img src={ArrowRight} />
+          </button>
+        </span>
       </section>
     </>
-  )}
-
+  );
+};
 
 export default Parceiros_big;
