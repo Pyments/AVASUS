0c3038601720c25846a1e221803e5b56ffc41738
Eduardo-Pyment <edupymota@protonmail.com>
2024-01-09 18:21:01 -0300

    ModulosEducacionais_big pagination done!

--- src/components/ModulosEducacionais_big.tsx
+++ src/components/ModulosEducacionais_big.tsx
@@ -1,6 +1,9 @@
 import { useState } from "react";
-import { useQuery } from "@tanstack/react-query";
-import { Link } from "react-router-dom";
+import { useQuery, keepPreviousData } from "@tanstack/react-query";
+// import { Link } from "react-router-dom";
+
+import ArrowLeft from "../assets/feather/arrow-left.svg";
+import ArrowRight from "../assets/feather/arrow-right.svg";
 
 import "../style/components/ModulosEducacionais_big.scss";
 
@@ -8,21 +11,27 @@ import Timer from "../assets/feather/clock.svg";
 import User from "../assets/feather/user.svg";
 import Star from "../assets/feather/star.svg";
 
-
 const ModulosEducacionais_big = () => {
-  const [modulosBigPage, setModulosBigPage] = useState("");
+  // const [modulosBigCategoria, setmodulosBigCategoria] = useState("")
+  const [modulosBigPage, setModulosBigPage] = useState(1);
+  const [modulosBigCategoria, setModulosBigCategoria] =
+    useState("Módulo de extensão");
 
-  const fetchModulosB = async (sort = "", page = "") => {
+  const fetchModulosBig = async (
+    modulosBigPage: number,
+    modulosBigCategoria: string
+  ) => {
     const response = await fetch(
-      `http://0.0.0.0:3004/cursos?_sort=${sort}&_page=${page}&_limit=9`
+      `http://0.0.0.0:3004/cursos?cateroria=${modulosBigCategoria}&_page=${modulosBigPage}&_limit=9`
     );
     const data = await response.json();
     return data;
   };
 
   const { isLoading, error, data, isFetching } = useQuery({
-    queryKey: ["queryModulosSmall", modulosBigPage],
-    queryFn: () => fetchModulosB(modulosBigPage),
+    queryKey: ["queryModulosBig", modulosBigPage, modulosBigCategoria],
+    queryFn: () => fetchModulosBig(modulosBigPage, modulosBigCategoria),
+    placeholderData: keepPreviousData,
   });
 
   if (isLoading || isFetching) {
@@ -44,20 +53,82 @@ const ModulosEducacionais_big = () => {
       <section className="modulosBig">
         <h3>Módulos Educacionais</h3>
         <div className="modulosBig-categorias">
-          <span>Covid 19</span>
-          <span>Sifilis e outras Ist's</span>
-          <span>Preceptoria</span>
-          <span>Doenças raras</span>
-          <span>Web Palestras</span>
-          <span>Sistemas prisionais</span>
-          <span>OPAS</span>
+          <button
+            onClick={() => {
+              setModulosBigCategoria("Especialização");
+            }}
+          >
+            Especialização
+          </button>
+          <button
+            onClick={() => {
+              setModulosBigCategoria("Módulo de extensão");
+            }}
+          >
+            Módulo de extensão
+          </button>
+          <button
+            onClick={() => {
+              setModulosBigCategoria("Síflis e outras ist");
+            }}
+          >
+            Síflis e outras ist
+          </button>
+          <button
+            onClick={() => {
+              setModulosBigCategoria("Covid 19");
+            }}
+          >
+            Covid 19
+          </button>
+          <button
+            onClick={() => {
+              setModulosBigCategoria("Preceptoria");
+            }}
+          >
+            Preceptoria
+          </button>
+          <button
+            onClick={() => {
+              setModulosBigCategoria("Doenças raras");
+            }}
+          >
+            Doenças raras
+          </button>
+          <button
+            onClick={() => {
+              setModulosBigCategoria("WebPalestras");
+            }}
+          >
+            WebPalestras
+          </button>
+          <button
+            onClick={() => {
+              setModulosBigCategoria("Acessibilidade");
+            }}
+          >
+            Acessibilidade
+          </button>
+          <button
+            onClick={() => {
+              setModulosBigCategoria("Sistema prisional");
+            }}
+          >
+            Sistema prisional
+          </button>
+          <button
+            onClick={() => {
+              setModulosBigCategoria("OPAS");
+            }}
+          >
+            OPAS
+          </button>
         </div>
-        <span className="modulosBig-resultados"># de # resultados</span>
-
+        {/* <span className="modulosBig-resultados">9 de {data.lenght} resultados</span> */}
         <ol className="modulosBig-container">
           {data?.map((data: any) => {
             return (
-              <li className="modulosBig-items">
+              <li key={data.id} className="modulosBig-items">
                 <img src={data?.capa} className="modulosBig-items-img"></img>
                 <h2 className="modulosBig-items-titulo">{data?.titulo}</h2>
                 <span className="modulosBig-items-parceiros">
@@ -65,13 +136,16 @@ const ModulosEducacionais_big = () => {
                 </span>
                 <div className="modulosBig-items-estatisticas">
                   <span className="modulosBig-items-matriculados">
-                    <img src={User} alt="Imagem matriculados" />{data?.matriculados}
+                    <img src={User} alt="Imagem matriculados" />
+                    {data?.matriculados}
                   </span>
                   <span className="modulosBig-items-duracao">
-                  <img src={Timer} alt="Imagem duração" />{data?.duracao}
+                    <img src={Timer} alt="Imagem duração" />
+                    {data?.duracao}
                   </span>
                   <span className="modulosBig-items-avaliacao">
-                  <img src={Star} alt="Imagem avaliação" />{data?.avaliacao}
+                    <img src={Star} alt="Imagem avaliação" />
+                    {data?.avaliacao}
                   </span>
                 </div>
                 <p className="modulosBig-items-objetivogeral">{data?.resumo}</p>
@@ -81,7 +155,20 @@ const ModulosEducacionais_big = () => {
           })}
         </ol>
         <div className="modulosBig-paginacao">
-          <button>paginação</button>
+          <button
+            onClick={() => setModulosBigPage((old) => Math.max(old - 1, 1))}
+            disabled={modulosBigPage === 1}
+          >
+            <img src={ArrowLeft} />
+          </button>
+          <span>{modulosBigPage}</span>
+          <button
+            onClick={() => {
+              setModulosBigPage((old) => old + 1);
+            }}
+          >
+            <img src={ArrowRight} />
+          </button>
         </div>
       </section>
     </>
