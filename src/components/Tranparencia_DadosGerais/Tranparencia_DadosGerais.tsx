d143e31318e53f022aefecc43b4d992ee52dfcd9
Eduardo-Pyment <edupymota@protonmail.com>
2024-01-10 13:26:37 -0300

    Transparencia(DadosGerais) Done

--- src/components/Tranparencia_DadosGerais/Tranparencia_DadosGerais.tsx
+++ src/components/Tranparencia_DadosGerais/Tranparencia_DadosGerais.tsx
@@ -1,7 +1,8 @@
 // import React from 'react'";
 import { useQuery } from "@tanstack/react-query";
+import "./Tranparencia_DadosGerais.scss";
 
-import Users from "../../assets/feather/clock.svg";
+import Users from "../../assets/feather/users.svg";
 import CheckSquare from "../../assets/feather/check-square.svg";
 import Play from "../../assets/feather/play.svg";
 import Award from "../../assets/feather/award.svg";
@@ -22,48 +23,77 @@ function Tranparencia_DadosGerais() {
 
   return (
     <section className="dados-gerais">
-      <h2>Dados Gerais</h2>
-      <div>
-        <span>
-          <img src={Users} />
-          <p>Total de usuários registrados</p>
-        </span>
-        <span>{data?.dados_gerais.usuarios_registrados}</span>
+      <h2 className="dados-gerais-title">Dados Gerais</h2>
+
+      <div className="dados-gerais-top">
+        <div className="dados-gerais-container">
+          <div>
+            <span className="dados-gerais-container-upper">
+              <img src={Users} />
+              <p>Total de usuários registrados</p>
+            </span>
+            <span className="dados-gerais-container-bottom">
+              {data?.dados_gerais.usuarios_registrados}
+            </span>
+          </div>
+        </div>
+        <div className="dados-gerais-container">
+          <div>
+            <span className="dados-gerais-container-upper">
+              <img src={CheckSquare} />
+              <p>Inscrições realizadas</p>
+            </span>
+            <span className="dados-gerais-container-bottom">
+              {data?.dados_gerais.incricoes_realizadas}
+            </span>
+          </div>
+        </div>
+        <div className="dados-gerais-container">
+          <div>
+            <span className="dados-gerais-container-upper">
+              <img src={Play} />
+              <p>Cursos ativos</p>
+            </span>
+            <span className="dados-gerais-container-bottom">
+              {data?.dados_gerais.cursos_ativos}
+            </span>
+          </div>
+        </div>
+        <div className="dados-gerais-container">
+          <div>
+            <span className="dados-gerais-container-upper">
+              <img src={Award} />
+              <p>Direito á Certificação</p>
+            </span>
+            <span className="dados-gerais-container-bottom">
+              {data?.dados_gerais.direito_certificacao}
+            </span>
+          </div>
+        </div>
       </div>
-      <div>
-        <span>
-          <img src={CheckSquare} />
-          <p>Inscrições realizadas</p>
-        </span>
-        <span>{data?.dados_gerais.incricoes_realizadas}</span>
-      </div>
-      <div>
-        <span>
-          <img src={Play} />
-          <p>Cursos ativos</p>
-        </span>
-        <span>{data?.dados_gerais.cursos_ativos}</span>
-      </div>
-      <div>
-        <span>
-          <img src={Award} />
-          <p>Direito á Certificação</p>
-        </span>
-        <span>{data?.dados_gerais.direito_certificacao}</span>
-      </div>
-      <div>
-        <span>
-          <img src={DollarSign} />
-          <p>Investimento médio por curso</p>
-        </span>
-        <span>{data?.dados_gerais.investimento_medio_curso}</span>
-      </div>
-      <div>
-        <span>
-          <img src={UserCheck} />
-          <p>Investimento médio por aluno</p>
-        </span>
-        <span>{data?.dados_gerais.investimento_medio_aluno}</span>
+      <div className="dados-gerais-bottom">
+        <div className="dados-gerais-container">
+          <div>
+            <span className="dados-gerais-container-upper">
+              <img src={DollarSign} />
+              <p>Investimento médio por curso</p>
+            </span>
+            <span className="dados-gerais-container-bottom">
+              {data?.dados_gerais.investimento_medio_curso}
+            </span>
+          </div>
+        </div>
+        <div className="dados-gerais-container">
+          <div>
+            <span className="dados-gerais-container-upper">
+              <img src={UserCheck} />
+              <p>Investimento médio por aluno</p>
+            </span>
+            <p className="dados-gerais-container-bottom">
+              {data?.dados_gerais.investimento_medio_aluno}
+            </p>
+          </div>
+        </div>
       </div>
     </section>
   );
